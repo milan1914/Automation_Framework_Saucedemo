56dcd2e7faa80e477e0891dfc8a5170c9f89883c
Milan.Zhekov <milanzhekov@gmail.com>
2022-03-23 20:58:54 +0200

    Next

--- src/test/java/qa/automation/ProductTests.java
+++ src/test/java/qa/automation/ProductTests.java
@@ -1,9 +1,22 @@
 package qa.automation;
 
 import base.TestUtil;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.collections.CollectionUtils;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 
 public class ProductTests extends TestUtil {
 
@@ -21,11 +34,27 @@ public class ProductTests extends TestUtil {
         WebElement loginBtn = driver.findElement(By.cssSelector("[value=Login]"));
         loginBtn.click();
 
-        //Thread.sleep(3000);
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         WebElement dropDownSortingOptions = driver.findElement(By.xpath("//select[@class='product_sort_container']"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(dropDownSortingOptions));
         dropDownSortingOptions.click();
+
+        FluentWait fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofSeconds(2))
+                .ignoreAll(Collections.singleton(NoSuchElementException.class));
         WebElement lowToHighPriceOption = driver.findElement(By.cssSelector("[value=lohi]"));
+
+        fluentWait.until(ExpectedConditions.elementToBeClickable(lowToHighPriceOption));
         lowToHighPriceOption.click();
         //Thread.sleep(3000);
+    }
+
+    @Test
+    public void addItemToTheCart(){
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productsPage = loginPage.login("standard_user","secret_sauce");
     }
 }
